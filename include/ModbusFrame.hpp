4045d0bcf88efcd0026cb98361a8307cf986f340
Krzysztof Dłużyński <k.dluzynski@sonictech.pl>
2025-12-05 14:55:59 +0100

    implement ModbusFrameView.hpp

--- include/ModbusFrame.hpp
+++ include/ModbusFrame.hpp
@@ -20,12 +20,11 @@ namespace eModbus {
 
 }
 namespace eModbus {
+    template <size_t MAX_MODBUS_FRAME_SIZE = 300>
     class Frame : public FrameView {
-        class Exception : std::exception {
-        };
 
     private:
-        std::array<uint8_t, 300> _internalDataBuffer = {0};
+        std::array<uint8_t, MAX_MODBUS_FRAME_SIZE> _internalDataBuffer = {0};
         std::span<uint8_t> _dataBuffer() override {
             return std::span<uint8_t> (_internalDataBuffer);
         }
@@ -35,68 +34,46 @@ namespace eModbus {
         }
 
     public:
+        explicit Frame(bool isRequest) : FrameView(Frame::_dataBuffer(),isRequest,true) {
+        };
+        explicit Frame(FrameView& view)
+            : FrameView(Frame::_dataBuffer(), view.isRequest(), true) // Initialize base class with Frame's buffer and FrameView's state
+        {
+            const std::span<const uint8_t> source_buffer = view.buffer();
+            const size_t copy_count = std::min(source_buffer.size(), _internalDataBuffer.size());
+            std::memcpy(_internalDataBuffer.data(), source_buffer.data(), copy_count);
+        }
 
-        Frame &setRawRtuData(std::span<uint8_t> RTU_Data, bool is_request, bool copy = true) {
-            //			if(copy){
+        Frame &setRawRtuData(std::span<uint8_t> RTU_Data, bool is_request) {
             isRequest(is_request);
             size_t copy_count = std::min(RTU_Data.size(), rtuBuffer().size());
             std::memcpy(rtuBuffer().data(), RTU_Data.data(), copy_count);
             MBAPLength(RTULengthWithoutCRC());
-            //			}else{
-            //				//TODO set flag that we are using external buffer for rtu buffer
-            //				// set rtu buffer span
-            //				_rtuDataBuffer = RTU_Data;
-            //			}
             return *this;
         }
 
-        Frame &setRawTcpData(std::span<const uint8_t> TCP_Data, bool is_request, bool copy = true) {
-            //			if(copy){
+        Frame &setRawTcpData(std::span<const uint8_t> TCP_Data, bool is_request) {
             isRequest(is_request);
             size_t copy_count = std::min(TCP_Data.size(), _dataBuffer().size());
             std::memcpy(_dataBuffer().data(), TCP_Data.data(), copy_count);
-            //			}else{
-            //				//TODO set flag that we are using external buffer
-            //				externalDataBuffer = true;
-            //				_dataBuffer() = TCP_Data;
-            //			}
             return *this;
         }
 
 
-        static Frame fromRawTcpData(std::span<const uint8_t> TCP_Data, bool isRequest, bool copy = true) {
+        static Frame fromRawTcpData(std::span<const uint8_t> TCP_Data, bool isRequest) {
             Frame result;
-            result.setRawTcpData(TCP_Data, isRequest, copy);
+            result.setRawTcpData(TCP_Data, isRequest);
             return result;
         }
 
-        static Frame fromRawRtuData(std::span<uint8_t> RTU_Data, bool isRequest, uint16_t transaction_ID = 0,
-                                          bool copy = true) {
+        static Frame fromRawRtuData(std::span<uint8_t> RTU_Data, bool isRequest) {
             Frame result;
-            result.setRawRtuData(RTU_Data, isRequest, copy);
+            result.setRawRtuData(RTU_Data, isRequest);
             return result;
-        }
-
-        Frame &clear() {
-            std::memset(_dataBuffer().data(), 0, _dataBuffer().size());
-            _dataBuffer() = std::span<uint8_t>(_internalDataBuffer);
-            _isRequest = false;
-            return *this;
         }
 
 
     };
 
-    static std::string to_string(const Frame::ValidationStatus status) {
-        switch (status) {
-            case Frame::ValidationStatus::OK:return "OK";
-            case Frame::ValidationStatus::InvalidCRC:return "Invalid CRC";
-            case Frame::ValidationStatus::InvalidFunctionCode:return "Invalid Function Code";
-            case Frame::ValidationStatus::ProtocolIdentifier:return "Protocol Identifier";
-            case Frame::ValidationStatus::MBAPHeaderLengthInvalid:return "MBAP Header Length Invalid";
-            default: return "Unknown";
-        }
-
-    }
 }
 #endif /* INC_MODBUS_HPP_ */
